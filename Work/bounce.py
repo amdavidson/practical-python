56bcea8b75df9f0bacc6c1bb0a6323d8c81e27d5
Andrew Davidson <andrew@amdavidson.com>
2020-06-26 15:32:21 -0400

    finished exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+current_height = 100
+bounce_factor = 3 / 5
+bounce_number = 1
+
+
+while bounce_number <= 10:
+    current_height = current_height * bounce_factor
+    print(bounce_number, round(current_height, 4))
+    bounce_number += 1
