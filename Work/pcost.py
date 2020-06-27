9947cc02fcd0c736c3249bd47822f6a46c09ce66
Andrew Davidson <andrew@amdavidson.com>
2020-06-26 21:12:12 -0400

    finished exercise 1.28

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+import gzip
+
+total_cost = 0
+
+with gzip.open("Data/portfolio.csv.gz", "rt") as f:
+    next(f)
+    for l in f:
+        [shares, price] = l.split(",")[1:]
+        total_cost = total_cost + int(shares) * float(price)
+
+print(total_cost)
