9ec4c7b8ce07d482b2a7a652b9843c4334465f04
Andrew Davidson <andrew@amdavidson.com>
2020-06-26 21:31:08 -0400

    exercise 1.32 complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,14 +3,23 @@
 # Exercise 1.27
 
 import os
-import gzip
+import csv
 
-total_cost = 0
 
-with gzip.open("Data/portfolio.csv.gz", "rt") as f:
-    next(f)
-    for l in f:
-        [shares, price] = l.split(",")[1:]
-        total_cost = total_cost + int(shares) * float(price)
+def portfolio_cost(filename):
+    "returns total cost of the portfolio from filename"
+    total_cost = 0
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        next(rows)
+        for l in rows:
+            try:
+                [shares, price] = l[1:]
+                total_cost = total_cost + int(shares) * float(price)
+            except:
+                print("Could not parse:", l)
+    return total_cost
 
-print(total_cost)
+
+total_cost = portfolio_cost("Data/missing.csv")
+print("Total cost:", total_cost)
